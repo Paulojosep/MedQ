442e2bee170682c38a6bbd93b591830f01dfefd3
Paulo Modesto <paulo.jmodesto@hotmail.com>
2022-02-11 12:28:20 -0300

    Conexao com Banco de Dados MySQL

---
+++ backend/MedQ.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MedQ.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedQ.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("MedQ.Application");
+            services.AddMediatR(myhandlers);
+
+            return services;
+        }
+    }
+}
