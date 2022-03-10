b71ffdcbc2b041ce7cda56c7ab3818f82c08c379
Paulojosep <paulo.jmodesto@hotmail.com>
2022-03-10 20:38:44 -0300

    Criacao do repositorio e service da fila

---
+++ backend/MedQ.Application/Services/FilaService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MedQ.Application.DTOs;
+using MedQ.Application.Interfaces;
+using MedQ.Domain.Entities;
+using MedQ.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Application.Services
+{
+    public class FilaService : IFilaService
+    {
+        private IFilaRepository _repository;
+        private readonly IMapper _mapper;
+
+        public FilaService(IFilaRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FilaDTO>> GetByEstabelecimentoAsync(int estabelecimentoId)
+        {
+            var filaEntity = await _repository.GetByEstabelecimentoAsync(estabelecimentoId);
+            var resultado = _mapper.Map< IEnumerable<FilaDTO>>(filaEntity);
+            return resultado;
+        }
+
+        public async Task<IEnumerable<FilaDTO>> GetByTipoAtendimentoAsync(int tipoAtendimentiId, int estabelecimentoId)
+        {
+            var filaEntity = await _repository.GetByTipoAtendimentoAsync(tipoAtendimentiId, estabelecimentoId);
+            var resultado = _mapper.Map<IEnumerable<FilaDTO>>(filaEntity);
+            return resultado;
+        }
+
+        public async Task<FilaDTO> CreateAsync(FilaDTO fila)
+        {
+            var filaEntity = _mapper.Map<Fila>(fila);
+            await _repository.CreateAsync(filaEntity);
+            var resultado = _mapper.Map<FilaDTO>(filaEntity);
+            return resultado;
+        }
+    }
+}
---
+++ backend/MedQ.Domain/Interfaces/IFilaRepository.cs
@@ -0,0 +1,15 @@
+﻿using MedQ.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Domain.Interfaces
+{
+    public interface IFilaRepository
+    {
+        Task<IEnumerable<Fila>> GetByEstabelecimentoAsync(int estabelecimentoId);
+        Task<IEnumerable<Fila>> GetByTipoAtendimentoAsync(int tipoAtendimentiId, int estabelecimentoId);
+        Task<Fila> CreateAsync(Fila fila);
+    }
+}
