bdbea9c7bb9116d4335ff93cddcbf1b4558e9cc0
Paulojosep <paulo.jmodesto@hotmail.com>
2022-03-21 19:11:11 -0300

    Classe MensagensController criada

--- backend/MedQ.Infra.Data/EntitiesConfiguration/MensagensConfiguration.cs
+++ backend/MedQ.Infra.Data/EntitiesConfiguration/MensagensConfiguration.cs
@@ -19,7 +19,7 @@ namespace MedQ.Infra.Data.EntitiesConfiguration
             builder.Property(p => p.Data).HasColumnName("data");
             builder.Property(p => p.Hora).HasColumnName("hora");
             builder.Property(p => p.Viewd).HasColumnName("viewed");
-            builder.Property(p => p.SocioId).HasColumnName("fk_socio").IsRequired();
+            builder.Property(p => p.SocioId).HasColumnName("fk_socio_id").IsRequired();
 
             builder.HasOne(s => s.Socio).WithMany(m => m.Mensagens).HasForeignKey(s => s.SocioId);
         }
