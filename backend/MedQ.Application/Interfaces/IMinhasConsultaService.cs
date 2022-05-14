f87eccb53f069ffef53418eb30d8b9c9dbf8fb36
Paulojosep <paulo.jmodesto@hotmail.com>
2022-05-14 18:23:19 -0300

    Criacao da interface e classe service

---
+++ backend/MedQ.Application/Interfaces/IMinhasConsultaService.cs
@@ -0,0 +1,17 @@
+﻿using MedQ.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Application.Interfaces
+{
+    public interface IMinhasConsultaService
+    {
+        Task<IEnumerable<MinhasConsultaDTO>> GetAll();
+        Task<MinhasConsultaDTO> GetFinishedMensagens(int socioId);
+        Task<MinhasConsultaDTO> CreateMyConsultation(MinhasConsultaDTO minhasConsulta);
+        Task<MinhasConsultaDTO> UpdateMyConsultation(MinhasConsultaDTO minhasConsulta);
+        Task DeleteMyConsultation(int id);
+    }
+}
---
+++ backend/MedQ.Application/Services/MinhasConsultaService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using MedQ.Application.DTOs;
+using MedQ.Application.Interfaces;
+using MedQ.Domain.Entities;
+using MedQ.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Application.Services
+{
+    public class MinhasConsultaService : IMinhasConsultaService
+    {
+        private IMinhasConsultaRepository _repository;
+        private readonly IMapper _mapper;
+
+        public MinhasConsultaService(IMinhasConsultaRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MinhasConsultaDTO>> GetAll()
+        {
+            var minhasConsultaEntity = await _repository.GetAllAsyns();
+            var resultado = _mapper.Map<IEnumerable<MinhasConsultaDTO>>(minhasConsultaEntity);
+            return resultado;
+        }
+
+        public async Task<MinhasConsultaDTO> GetFinishedMensagens(int socioId)
+        {
+            var minhasConsultaEntity = await _repository.GetFinishedMensagensAsync(socioId);
+            var resultado = _mapper.Map<MinhasConsultaDTO>(minhasConsultaEntity);
+            return resultado;
+        }
+
+        public async Task<MinhasConsultaDTO> CreateMyConsultation(MinhasConsultaDTO minhasConsulta)
+        {
+            var minhasConsultaEntity = _mapper.Map<MinhasConsulta>(minhasConsulta);
+            minhasConsultaEntity.Finished = MakeId();
+            await _repository.CreateMyConsultationAsync(minhasConsultaEntity);
+            var resultado = _mapper.Map<MinhasConsultaDTO>(minhasConsultaEntity);
+            return resultado;
+        }
+
+        public async Task<MinhasConsultaDTO> UpdateMyConsultation(MinhasConsultaDTO minhasConsulta)
+        {
+            var minhasConsultaEntity = _mapper.Map<MinhasConsulta>(minhasConsulta);
+            await _repository.UpdateMyConsultationAsync(minhasConsultaEntity);
+            var resultado = _mapper.Map<MinhasConsultaDTO>(minhasConsultaEntity);
+            return resultado;
+        }
+
+        public async Task DeleteMyConsultation(int id)
+        {
+            await _repository.DeleteMyConsultationAsync(id);
+        }
+
+        private string MakeId()
+        {
+            Random random = new Random();
+            var pedido = "";
+            var possibilidade = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for(int i = 0; i < 16; i++)
+            {
+                possibilidade = Convert.ToString(Math.Floor(random.NextDouble() * possibilidade.Length));
+                pedido += possibilidade;
+            }
+            return pedido;
+        }
+    }
+}
