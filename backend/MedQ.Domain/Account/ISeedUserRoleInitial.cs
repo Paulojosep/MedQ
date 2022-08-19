43b3dc380262235b309dfe5926adb88ff944c316
Paulo Modesto <paulo.jmodesto@hotmail.com>
2022-08-19 18:33:40 -0300

    authenticate

---
+++ backend/MedQ.Domain/Account/ISeedUserRoleInitial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedQ.Domain.Account
+{
+    public interface ISeedUserRoleInitial
+    {
+        void SeedUser();
+        void SeedRoles();
+    }
+}
---
+++ backend/MedQ.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedQ.Domain.Entities
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {}
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
