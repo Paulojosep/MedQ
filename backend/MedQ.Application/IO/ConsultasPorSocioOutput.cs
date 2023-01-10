b3876cd0defbace96e5e60b93f4dbea09ec9dc3f
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-01-10 12:17:50 -0300

    Criacao da classe output

---
+++ backend/MedQ.Application/IO/ConsultasPorSocioOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedQ.Application.IO
+{
+    public class ConsultasPorSocioOutput
+    {
+        public string Medico { get; set; }
+        public string Especialidade { get; set; }
+        public string Consultas { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public string Status { get; set; }
+        public string Senha { get; set; }
+    }
+}
