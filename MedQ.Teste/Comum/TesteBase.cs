1157e86b32a361ce1139a0a9bee35decd6df9c32
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-06-22 23:51:53 -0300

    Criacao do primeiro teste

---
+++ MedQ.Teste/Comum/TesteBase.cs
@@ -0,0 +1,50 @@
+﻿using MedQ.Infra.Data.Context;
+using MedQ.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Teste.Comum
+{
+    public abstract class TesteBase
+    {
+        private static object Objeto = new object();
+        private static MedQContext _context = default!;
+        protected static MedQContext Context
+        {
+            get
+            {
+                lock(Objeto)
+                {
+                    if (_context == null)
+                    {
+                        return _context;
+                    }
+                    return CriarInstancia();
+                }
+            }
+        }
+
+
+        private static MedQContext CriarInstancia()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+             .AddJsonFile(path: "appsettings.json");
+            IConfiguration _config = builder.Build();
+
+            DbContextOptionsBuilder<MedQContext> dbContext = new DbContextOptionsBuilder<MedQContext>();
+            dbContext.UseSqlite(_config.GetConnectionString("padrao"));
+            _context = new MedQContext(dbContext.Options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            SEED.Popular(_context);
+            return _context;
+        }
+
+    }
+}
---
+++ MedQ.Teste/Entities/AgendamentoDisponiveisTeste.cs
@@ -0,0 +1,21 @@
+﻿using MedQ.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MedQ.Teste.Entities
+{
+    public class AgendamentoDisponiveisTeste
+    {
+        [Fact(DisplayName = "Adicionar Agendamento Disponivel")]
+        public void AdicionarAgendamentoDisponivel()
+        {
+            // Act
+            Action action = () => new AgendamentoDisponiveis(1, 1, 1, 1, DateTime.Now, DateTime.Now);
+            
+        }
+    }
+}
