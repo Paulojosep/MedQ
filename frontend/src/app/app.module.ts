17d42810ce28d36bd72b13645a86e70a12f4aeb9
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-07-10 20:51:57 -0300

    criacao do projeto front V3.2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AppLayoutModule } from './layout/app.layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppLayoutModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
