8c5fb420766c0cb6cd33c404dcbd3e93da6a1844
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-08-03 20:31:41 -0300

    Alteracao no login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginModule } from './core/authentication/login/login.module';
 import { LayoutModule } from './modules/layout/layout.module';
 import { ConsultasModule } from './modules/private/consultas/consultas.module';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { GuardaRotaService } from './core/services/guarda-rota.service';
 import { AlertModule } from './shared/alert/alert.module';
 import { FormsModule } from '@angular/forms';
 import { HospitalModule } from './modules/private/hospital/hospital.module';
+import { ErrorInterceptor } from './core/util/error-interceptor';
+import { TokenInterceptor } from './core/util/token-interceptor';
 
 
 
@@ -35,7 +37,10 @@ import { HospitalModule } from './modules/private/hospital/hospital.module';
     AppRoutingModule,
     AlertModule
   ],
-  providers: [GuardaRotaService],
+  providers: [
+    GuardaRotaService,
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
