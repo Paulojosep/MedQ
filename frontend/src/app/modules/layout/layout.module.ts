f5b75ac0c1c9f3d97dc7514756f9659606a8df32
Paulo Modesto <paulo.jmodesto@hotmail.com>
2023-12-26 11:42:10 -0300

    Alteracao Layout -incompleto

--- frontend/src/app/modules/layout/layout.module.ts
+++ frontend/src/app/modules/layout/layout.module.ts
@@ -3,20 +3,29 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import {MatSidenavModule} from '@angular/material/sidenav'
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
     declarations: [
-        HeaderComponent,
-        FooterComponent
+      HeaderComponent,
+      FooterComponent,
+      NavBarComponent
     ],
     imports: [
       BrowserModule,
-      RouterModule
+      RouterModule,
+      MatSidenavModule,
+      MatListModule,
+      MatToolbarModule
     ],
     providers: [],
     exports: [
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NavBarComponent
     ]
   })
   export class LayoutModule { }
