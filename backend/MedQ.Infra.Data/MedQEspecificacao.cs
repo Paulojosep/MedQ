2f86a2a81a902df0a2b168e77cc09e981f763212
Paulo Modesto <paulo.jmodesto@hotmail.com>
2024-01-16 12:20:36 -0300

    Criacao do metodo PaginarAsync

--- backend/MedQ.Infra.Data/MedQEspecificacao.cs
+++ backend/MedQ.Infra.Data/MedQEspecificacao.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MedQ.Infra.Data
 {
@@ -28,6 +29,24 @@ namespace MedQ.Infra.Data
         {
             var query = source.IQueryable();
             return query.Where(parametroConsulta);
+        }
+
+        public static async Task<ListaPaginada<T>> PaginarAsync<T>(this IQueryable<T> query, ParametroPaginacao<T> filtro)
+        {
+            var quantidade = await query.CountAsync();
+
+            if (!string.IsNullOrEmpty(filtro.OrderBy))
+            {
+                string orderBy = filtro.OrderBy[0].ToString().ToUpper() + filtro.OrderBy.Substring(1);
+                if (filtro.Ordem.Equals(1))
+                    query = query.OrderByDescending(p => Microsoft.EntityFrameworkCore.EF.Property<T>(p!, orderBy));
+                else
+                    query = query.OrderBy(p => Microsoft.EntityFrameworkCore.EF.Property<T>(p!, orderBy));
+            }
+
+            var lista = await query.Skip(filtro.PaginaAtual() * filtro.Tamanho).Take(filtro.Tamanho).ToListAsync();
+
+            return new ListaPaginada<T>(lista);
         }
     }
 }
