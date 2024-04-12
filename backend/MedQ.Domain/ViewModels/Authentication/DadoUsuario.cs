aa6019f4c9a989492bdd2919ecd23a56a26923fb
Paulo Modesto <paulo.jmodesto@hotmail.com>
2024-04-12 12:23:54 -0300

    Criacao do servico AutheticationService

---
+++ backend/MedQ.Domain/ViewModels/Authentication/DadoUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedQ.Domain.ViewModels.Authentication
+{
+    public class DadoUsuario
+    {
+        public Usuario Usuario { get; set;}
+        public List<Menu> Menus { get; set;}
+    }
+}
