bd02a6d3eb554e7d66ec736631f9481d2a3b00c4
Paulo Modesto <paulo.jmodesto@hotmail.com>
2026-04-15 09:22:01 -0300

    Criacao da nova coluna latitude e longitude da tablea de banco de dados e alteracoa de versão dos pacotes

---
+++ backend/MedQ.Infra.Data/Migrations/20260415121852_AddLatitudeLongitude.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedQ.Infra.Data.Migrations
+{
+    public partial class AddLatitudeLongitude : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "latitude",
+                table: "tb_estabelecimento",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "longitude",
+                table: "tb_estabelecimento",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "latitude",
+                table: "tb_estabelecimento");
+
+            migrationBuilder.DropColumn(
+                name: "longitude",
+                table: "tb_estabelecimento");
+        }
+    }
+}
